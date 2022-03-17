c2d0009c506d3bcf9212347c3d2a7e4f5e547e75
Sachin Shelke <shelkesp1@gmail.com>
2022-03-17 08:50:05 +0530

    products_detail-page-dynamic

--- src/components/main/components/MenuBar.jsx
+++ src/components/main/components/MenuBar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MenuBar = () => {
   return (
-    <div className="z-10 fixed top-16 left-0 right-0 bg-[#c8d8e4] text-[#2b6777] p-2">
+    <div className="z-10 fixed top-16 left-0 right-0 bg-[#c8d8e4] text-[#2b6777] p-2 2xl:container mx-auto">
       <div className="flex gap-5">
         <p>Categories</p>
         <p>Mobiles</p>
